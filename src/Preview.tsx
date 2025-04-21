6e59be4b44f867b64a309b3557de83fd2c2fe7d1
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-04-21 21:45:54 +0200

    back to basics

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -1,10 +1,10 @@
 function Preview () {
   return (
-    <div>
+    <>
       <h1>Preview</h1>
       <p>This is the preview component.</p>
       <p>It will show the preview of the editor content.</p>
-    </div>
+    </>
   );
 }
 export default Preview;
